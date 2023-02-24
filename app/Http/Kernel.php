23098ed044c5f9565853cbda7640f602ab3f5e44
behnamzgh <behnamzgh@gmail.com>
2023-02-24 20:42:06 +0330

    masire middleware jadid ro to kernel ghesmate routeMiddleware ezafe kardim va esme IsAdmin gozashtim baraye farakhani

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -54,6 +54,8 @@ class Kernel extends HttpKernel
      */
     protected $routeMiddleware = [
         'auth' => \App\Http\Middleware\Authenticate::class,
+        // ezafe kardan middleware IsAdmin b kernel
+        'IsAdmin' => \App\Http\Middleware\IsAdmin::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'auth.session' => \Illuminate\Session\Middleware\AuthenticateSession::class,
         'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
