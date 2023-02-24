9073e981b9b39d611e2b338f4ef2081ce07e253e
behnamzgh <behnamzgh@gmail.com>
2023-02-24 19:21:51 +0330

    ezafe kardan sweetalert ba confirmation baraye hazf karbar

--- resources/views/panel/users/index.blade.php
+++ resources/views/panel/users/index.blade.php
@@ -34,28 +34,29 @@
                 <tbody>
                     @foreach ($users as $user)
                         <tr role="row" class="">
-                            <td>{{$user->id}}</td>
-                            <td>{{$user->name}}</td>
-                            <td>{{$user->phone}}</td>
-                            <td>{{$user->email}}</td>
-                            <td>{{$user->getUserRoleInFarsi()}}</td>
-                            <td>{{$user->jalali_created_at()}}</td>
-                            <td>{{$user->jalali_updated_at()}}</td>
+                            <td>{{ $user->id }}</td>
+                            <td>{{ $user->name }}</td>
+                            <td>{{ $user->phone }}</td>
+                            <td>{{ $user->email }}</td>
+                            <td>{{ $user->getUserRoleInFarsi() }}</td>
+                            <td>{{ $user->jalali_created_at() }}</td>
+                            <td>{{ $user->jalali_updated_at() }}</td>
                             <td>
 
                                 {{-- in if baraye inke faghat admin betone tavanaii delete dashte bashe va --}}
                                 {{-- karbari k login karde natone khodesh ro delete kone --}}
-                                @if(auth()->user()->role === 'admin' && auth()->user()->id !== $user->id )
-
-                                {{-- 2ta meghdar ba onlick pass midim yekish event k hamon click karbar hast --}}
-                                {{-- dovomi id user k ersal mishe b function deleteUser on paiin --}}
-                                <a href="" class="item-delete mlg-15" title="حذف" onclick="deleteUser(event,{{$user->id}})"></a>
+                                @if (auth()->user()->role === 'admin' && auth()->user()->id !== $user->id)
+                                    {{-- 2ta meghdar ba onlick pass midim yekish event k hamon click karbar hast --}}
+                                    {{-- dovomi id user k ersal mishe b function deleteUser on paiin --}}
+                                    <a href="" class="item-delete mlg-15" title="حذف"
+                                        onclick="deleteUser(event,{{ $user->id }})"></a>
                                 @endif
 
                                 <a href="{{ route('users.edit', $user->id) }}" class="item-edit " title="ویرایش"></a>
 
                                 {{-- inja va on paiin agar to id {{$user->id}} ro nazarim modam tekrari mishe va karbar ro tashkhis nemide --}}
-                                <form action="{{route('users.destroy', $user->id)}}" method="post" id="delete-user-{{$user->id}}">
+                                <form action="{{ route('users.destroy', $user->id) }}" method="post"
+                                    id="delete-user-{{ $user->id }}">
                                     @csrf
                                     @method('delete')
                                 </form>
@@ -64,14 +65,27 @@
                     @endforeach
                 </tbody>
             </table>
-            {{$users->links()}}
+            {{ $users->links() }}
         </div>
     </div>
     <x-slot name="scripts">
         <script>
-            function deleteUser(event, id){
-                event.preventDefault()
-                document.getElementById('delete-user-' + id).submit()
+            function deleteUser(event, id) {
+                event.preventDefault();
+                Swal.fire({
+                    title: 'آیا مطمئن هستید؟',
+                    text: "بعد از حذف قادر به بازگردانی نیستید...",
+                    icon: 'warning',
+                    showCancelButton: true,
+                    confirmButtonColor: '#d33',
+                    cancelButtonColor: '#3085d6',
+                    confirmButtonText: 'حذف',
+                    cancelButtonText: 'پشیمون شدم'
+                }).then((result) => {
+                    if (result.isConfirmed) {
+                        document.getElementById('delete-user-' + id).submit();
+                    }
+                })
             }
         </script>
     </x-slot>
