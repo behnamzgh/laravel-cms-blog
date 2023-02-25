1d0bef4546461f3986cabf97fffe123b5869dcfe
behnamzgh <behnamzgh@gmail.com>
2023-02-25 17:04:22 +0330

    ijad route group /panel b hamrah middleware

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Panel\CategoryController;
 use App\Http\Controllers\Panel\UserController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
@@ -13,14 +14,16 @@ Route::get('/post/{id}', function () {
     return view('post');
 })->name('post.show');
 
-Route::middleware('auth')->get('/karbar', fn() => 'profile')->name('karbar');
+Route::middleware('auth')->get('/karbar', fn () => 'profile')->name('karbar');
 
 Route::get('/dashboard', function () {
     return view('panel.index');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-// middlware auth baraye inke faghat karbarani k login bashand betonan varede masire panel/users beshan
-Route::middleware(['auth', 'IsAdmin'])->resource('panel/users',UserController::class)->except(['show']);
+Route::middleware(['auth', 'IsAdmin'])->prefix('/panel')->group(function () {
+    Route::resource('/users', UserController::class)->except(['show']);
+    Route::resource('/categories', CategoryController::class)->except(['show']);
+});
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
