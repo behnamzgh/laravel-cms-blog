89a6feb185db2bd36afbcd82b93989d1af520810
behnamzgh <behnamzgh@gmail.com>
2023-02-26 01:07:20 +0330

    validate ba estafade az request ha

--- app/Http/Controllers/Panel/CategoryController.php
+++ app/Http/Controllers/Panel/CategoryController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Panel;
 use App\Http\Controllers\Controller;
 use App\Models\Category;
 use Illuminate\Http\Request;
+use App\Http\Requests\Panel\Category\CreateCategoryRequest;
+use App\Http\Requests\Panel\Category\UpdateCategoryRequest;
 
 class CategoryController extends Controller
 {
@@ -16,39 +18,41 @@ class CategoryController extends Controller
         return \view('panel.categories.index', \compact('categories', 'parentCategories'));
     }
 
-    public function store(Request $request)
+    public function store(CreateCategoryRequest $request)
     {
-        // 1.validate
-        $request->validate([
-            'name' => ['required', 'max:255'],
-            'slug' => ['required', 'max:255', 'unique:categories'],
-            'category_id' => ['nullable', 'exists:categories,id']
-        ]);
-
-        // 2.save
         Category::create(
-            $request->only(['name', 'slug', 'category_id'])
+            $request->validated()
         );
 
-        // 3.session
         \session()->flash('status', 'دسته بندی ذخیره شد');
 
-        // 4.return
         return \back();
     }
 
     public function edit(Category $category)
     {
-        //
+        // inja migim az model category faghat onaii k category_id shon null hast yani parent hastan hamchenin
+        // id sho barabar nist ba id on araye(category) k az form ersal shode va ba route model binding gereftim ro behemon bargardone
+        $parentCategories = Category::where('category_id', null)->where('id', '!=', $category->id)->get();
+        return \view('panel.categories.edit', \compact('category', 'parentCategories'));
     }
 
-    public function update(Request $request, Category $category)
+    public function update(UpdateCategoryRequest $request, Category $category)
     {
-        //
+        $category->update(
+            $request->validated()
+        );
+
+        \session()->flash('status', 'دسته بندی به روز شد');
+
+        return \redirect()->route('categories.index');
     }
 
     public function destroy(Category $category)
     {
-        //
+        // ba estefade az route model binding tashkhis mide id k az taraf blade ersal shode va hamon category khas ro delete mikone
+        $category->delete();
+        \session()->flash('status', 'دسته بندی حذف شد');
+        return \back();
     }
 }
