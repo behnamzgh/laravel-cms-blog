47f1bfcf41d846aebd1a34983d4dfb7fc93a674a
behnamzgh <behnamzgh@gmail.com>
2023-02-25 19:41:11 +0330

    takmil safhe index category ha

--- resources/views/panel/categories/index.blade.php
+++ resources/views/panel/categories/index.blade.php
@@ -4,8 +4,8 @@
     </x-slot>
     <div class="breadcrumb">
         <ul>
-            <li><a href="{{route('dashboard')}}">پیشخوان</a></li>
-            <li><a href="{{route('categories.index')}}" class="is-active">دسته بندی ها</a></li>
+            <li><a href="{{ route('dashboard') }}">پیشخوان</a></li>
+            <li><a href="{{ route('categories.index') }}" class="is-active">دسته بندی ها</a></li>
         </ul>
     </div>
     <div class="main-content padding-0 categories">
@@ -24,30 +24,47 @@
                             </tr>
                         </thead>
                         <tbody>
-                            <tr role="row" class="">
-                                <td><a href="">1</a></td>
-                                <td><a href="">برنامه نویسی</a></td>
-                                <td>programming</td>
-                                <td>ندارد</td>
-                                <td>
-                                    <a href="" class="item-delete mlg-15" title="حذف"></a>
-                                    <a href="edit-category.html" class="item-edit " title="ویرایش"></a>
-                                </td>
-                            </tr>
+                            @foreach ($categories as $category)
+                                <tr role="row" class="">
+                                    <td>{{ $category->id }}</td>
+                                    <td>{{ $category->name }}</td>
+                                    <td>{{ $category->slug }}</td>
+                                    <td>{{ $category->getParentName() }}</td>
+                                    <td>
+                                        <a href="" class="item-delete mlg-15" title="حذف"></a>
+                                        <a href="edit-category.html" class="item-edit " title="ویرایش"></a>
+                                    </td>
+                                </tr>
+                            @endforeach
                         </tbody>
                     </table>
+                    {{$categories->links()}}
                 </div>
             </div>
             <div class="col-4 bg-white">
                 <p class="box__title">ایجاد دسته بندی جدید</p>
-                <form action="" method="post" class="padding-30">
-                    <input type="text" placeholder="نام دسته بندی" class="text">
-                    <input type="text" placeholder="نام انگلیسی دسته بندی" class="text">
+                <form action="{{ route('categories.store') }}" method="post" class="padding-30">
+                    @csrf
+
+                    <input type="text" name="name" placeholder="نام دسته بندی" class="text">
+                    @error('name')
+                        <p class="error">{{ $message }}</p>
+                    @enderror
+
+                    <input type="text" name="slug" placeholder="نام انگلیسی دسته بندی" class="text">
+                    @error('slug')
+                        <p class="error">{{ $message }}</p>
+                    @enderror
+
                     <p class="box__title margin-bottom-15">انتخاب دسته پدر</p>
-                    <select class="select" name="" id="">
-                        <option value="0">ندارد</option>
-                        <option value="0">برنامه نویسی</option>
+
+                    <select class="select" name="category_id" id="">
+                        <option value="">ندارد</option>
+                        @foreach ($parentCategories as $parentCategory)
+                            <option value="{{ $parentCategory->id }}">{{ $parentCategory->name }}</option>
+                        @endforeach
                     </select>
+
                     <button class="btn btn-webamooz_net">اضافه کردن</button>
                 </form>
             </div>
