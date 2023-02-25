8da93bee4dad0b9998a5a42098224c29137dfe93
behnamzgh <behnamzgh@gmail.com>
2023-02-25 17:01:47 +0330

    daste bandi ha dar pishkhan sidebar faghat baraye admin ha namayesh dade beshe

--- resources/views/layouts/panel.blade.php
+++ resources/views/layouts/panel.blade.php
@@ -32,8 +32,8 @@
             {{-- shart mizarim k karbar admin nabod natone b tab users dastresi dashte bashe --}}
             @if(auth()->user()->role === 'admin')
             <li class="item-li i-users @if (request()->is('panel/users') || request()->is('panel/users/*')) is-active @endif"><a href="{{ route('users.index') }}"> کاربران</a></li>
+            <li class="item-li i-categories @if (request()->is('panel/categories') || request()->is('panel/categories/*')) is-active @endif"><a href="{{route('categories.index')}}">دسته بندی ها</a></li>
             @endif
-            <li class="item-li i-categories"><a href="categories.html">دسته بندی ها</a></li>
             <li class="item-li i-articles"><a href="articles.html">مقالات</a></li>
             <li class="item-li i-comments"><a href="comments.html"> نظرات</a></li>
             <li class="item-li i-user__inforamtion"><a href="user-information.html">اطلاعات کاربری</a></li>
