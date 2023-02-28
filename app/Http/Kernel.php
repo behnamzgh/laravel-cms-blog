d0158b465d8d745d597aa494874b855c922eeaa7
behnamzgh <behnamzgh@gmail.com>
2023-02-28 18:55:23 +0330

    ezafe kardan middleware IsAuthor

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -54,8 +54,9 @@ class Kernel extends HttpKernel
      */
     protected $routeMiddleware = [
         'auth' => \App\Http\Middleware\Authenticate::class,
-        // ezafe kardan middleware IsAdmin b kernel
+        // ezafe kardan middleware IsAdmin va IsAuthor b kernel
         'IsAdmin' => \App\Http\Middleware\IsAdmin::class,
+        'IsAuthor' => \App\Http\Middleware\IsAuthor::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'auth.session' => \Illuminate\Session\Middleware\AuthenticateSession::class,
         'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
