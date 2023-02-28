3929c1c48ede45c7047c2bbcd92bfe9e263e4aad
behnamzgh <behnamzgh@gmail.com>
2023-02-28 18:59:22 +0330

    fillable - relations - tarikh farsi

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,31 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'banner',
+        'content',
+        'user_id',
+    ];
+
+    public function categories()
+    {
+        return $this->belongsToMany(Category::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    // function tabdil tarikh miladi b jalali
+    public function jalali_created_at()
+    {
+        return verta($this->created_at)->format('Y/m/d');
+    }
+    public function jalali_updated_at()
+    {
+        return verta($this->updated_at)->format('Y/m/d');
+    }
 }
