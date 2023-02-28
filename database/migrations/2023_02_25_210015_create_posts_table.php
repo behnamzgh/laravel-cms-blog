e635c8f21cc0d6ef792d77df5e4bc9d69a325658
behnamzgh <behnamzgh@gmail.com>
2023-02-28 12:13:08 +0330

    ijad migration post va table haye mored niaz

--- database/migrations/2023_02_25_210015_create_posts_table.php
+++ database/migrations/2023_02_25_210015_create_posts_table.php
@@ -15,6 +15,10 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->string('slug');
+            $table->string('image');
+            $table->string('content');
             $table->timestamps();
         });
     }
