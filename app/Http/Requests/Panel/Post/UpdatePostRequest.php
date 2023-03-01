06d48c542ccae0c2cda6f8dce60d27114681fbd0
behnamzgh <behnamzgh@gmail.com>
2023-03-01 23:16:59 +0330

    ijad request baraye update post

---
+++ app/Http/Requests/Panel/Post/UpdatePostRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests\Panel\Post;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdatePostRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'title' => ['required', 'max:255', 'string'],
+            'categories' => ['required', 'array'],
+            'categories.*' => ['required', 'string'],
+            'banner' => ['nullable', 'image'],
+            'content' => ['required']
+        ];
+    }
+}
