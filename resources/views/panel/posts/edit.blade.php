9e166644a1af1d0a3de4ee0567703f811af83183
behnamzgh <behnamzgh@gmail.com>
2023-03-01 23:17:36 +0330

    takmil blade edit post

--- resources/views/panel/posts/edit.blade.php
+++ resources/views/panel/posts/edit.blade.php
@@ -6,45 +6,73 @@
         <ul>
             <li><a href="{{ route('dashboard') }}">پیشخوان</a></li>
             <li><a href="{{ route('posts.index') }}"> مقالات</a></li>
-            <li><a href="{{ route('posts.edit', 1) }}" class="is-active">ویرایش مقاله</a></li>
+            <li><a href="{{ route('posts.edit', $post->id) }}" class="is-active">ویرایش مقاله</a></li>
         </ul>
     </div>
     <div class="main-content padding-0">
         <p class="box__title">ویرایش مقاله</p>
         <div class="row no-gutters bg-white">
             <div class="col-12">
-                <form action="" class="padding-30">
-                    <input type="text" class="text" placeholder="عنوان مقاله">
-                    <input type="text" class="text text-left " placeholder="نام انگلیسی مقاله">
+                <form action="{{ route('posts.update', $post->id) }}" class="padding-30" method="post"
+                    enctype="multipart/form-data">
+                    @csrf
+                    @method('put')
 
+                    <input type="text" name="title" class="text" placeholder="عنوان مقاله"
+                        value="{{ $post->title }}">
+                    @error('title')
+                        <p class="error">{{ $message }}</p>
+                    @enderror
+
+                    {{-- categories --}}
                     <ul class="tags">
-
-                        <li class="addedTag">dsfsdf<span class="tagRemove" onclick="$(this).parent().remove();">x</span>
-                            <input type="hidden" value="dsfsdf" name="tags[]">
-                        </li>
-                        <li class="addedTag">dsfsdf<span class="tagRemove" onclick="$(this).parent().remove();">x</span>
-                            <input type="hidden" value="dsfsdf" name="tags[]">
-                        </li>
+                        @foreach ($post->categories as $category)
+                            <li class="addedTag">{{ $category->name }}<span class="tagRemove" onclick="$(this).parent().remove();">x</span>
+                                <input type="hidden" value="{{ $category->name }}" name="categories[]">
+                            </li>
+                        @endforeach
                         <li class="tagAdd taglist">
                             <input type="text" id="search-field">
                         </li>
                     </ul>
+                    @error('categories')
+                        <p class="error">{{ $message }}</p>
+                    @enderror
 
+                    {{-- banner --}}
                     <div class="file-upload">
                         <div class="i-file-upload">
                             <span>آپلود بنر دوره</span>
-                            <input type="file" class="file-upload" id="files" name="files" />
+                            <input type="file" class="file-upload" id="files" name="banner" />
                         </div>
                         <span class="filesize"></span>
                         <span class="selectedFiles">فایلی انتخاب نشده است</span>
                     </div>
-                    <textarea placeholder="متن مقاله" class="text "></textarea>
+
+                    {{-- content --}}
+                    <textarea placeholder="متن مقاله" class="text" name="content">{!! $post->content !!}</textarea>
+                    @error('content')
+                        <p class="error">{{ $message }}</p>
+                    @enderror
+
                     <button class="btn btn-webamooz_net">ویرایش مقاله</button>
                 </form>
             </div>
         </div>
     </div>
     <x-slot name="scripts">
+        <script src="https://cdn.ckeditor.com/4.20.2/standard/ckeditor.js"></script>
+        <script>
+            CKEDITOR.replace('content', {
+                language: 'fa',
+                filebrowserUploadUrl: '{{ route('upload-file', ['_token' => csrf_token()]) }}',
+                filebrowserUploadMethod: 'form',
+            })
+        </script>
+
+        {{-- hamin tike jquery paiin k ezafe nashode bod ghesmate tag input form aslan kar nemikard! --}}
+        {{-- <script src="http://code.jquery.com/jquery-1.11.0.min.js"></script> --}}
+        <script src="{{ asset('blog/panel/js/jquery-3.4.1.min.js') }}"></script>
         <script src="{{ asset('blog/panel/js/tagsInput.js') }}"></script>
     </x-slot>
 </x-panel-layout>
