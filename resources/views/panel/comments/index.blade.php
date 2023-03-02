7932ab4a1dbfc3d53a17b80515d213c1eb362c72
behnamzgh <behnamzgh@gmail.com>
2023-03-03 01:18:05 +0330

    blade comment index

---
+++ resources/views/panel/comments/index.blade.php
@@ -0,0 +1,73 @@
+<x-panel-layout>
+<x-slot name="title">
+    - مدیریت نظرات
+</x-slot>
+    <div class="breadcrumb">
+        <ul>
+            <li><a href="index.html">پیشخوان</a></li>
+            <li><a href="comments.html" class="is-active">نظرات</a></li>
+        </ul>
+    </div>
+    <div class="main-content">
+        <div class="tab__box">
+            <div class="tab__items">
+                <a class="tab__item is-active" href="comments.html"> همه نظرات</a>
+                <a class="tab__item " href="comments.html">نظرات تاییده نشده</a>
+                <a class="tab__item " href="comments.html">نظرات تاییده شده</a>
+            </div>
+        </div>
+
+
+        <div class="table__box">
+            <table class="table">
+                <thead role="rowgroup">
+                    <tr role="row" class="title-row">
+                        <th>شناسه</th>
+                        <th>ارسال کننده</th>
+                        <th>برای</th>
+                        <th>دیدگاه</th>
+                        <th>تاریخ</th>
+                        <th>تعداد پاسخ ها</th>
+                        <th>وضعیت</th>
+                        <th>عملیات</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr role="row">
+                        <td><a href="">1</a></td>
+                        <td><a href="">محمد نیکو</a></td>
+                        <td><a href=""> لاراول</a></td>
+                        <td>دوره خوبی بود</td>
+                        <td>1399/05/01</td>
+                        <td>13</td>
+                        <td class="text-success">تاییده شده</td>
+                        <td>
+                            <a href="" class="item-delete mlg-15" title="حذف"></a>
+                            <a href="show-comment.html" class="item-reject mlg-15" title="رد"></a>
+                            <a href="show-comment.html" target="_blank" class="item-eye mlg-15" title="مشاهده"></a>
+                            <a href="show-comment.html" class="item-confirm mlg-15" title="تایید"></a>
+                            <a href="edit-comment.html" class="item-edit " title="ویرایش"></a>
+                        </td>
+                    </tr>
+                    <tr role="row">
+                        <td><a href="">1</a></td>
+                        <td><a href="">محمد نیکو</a></td>
+                        <td><a href="">دوره لاراول</a></td>
+                        <td>دوره خوبی بود</td>
+                        <td>1399/05/01</td>
+                        <td>13</td>
+                        <td class="text-error">تاییده نشده</td>
+                        <td>
+                            <a href="" class="item-delete mlg-15" title="حذف"></a>
+                            <a href="show-comment.html" class="item-reject mlg-15" title="رد"></a>
+                            <a href="show-comment.html" target="_blank" class="item-eye mlg-15" title="مشاهده"></a>
+                            <a href="show-comment.html" class="item-confirm mlg-15" title="تایید"></a>
+                            <a href="edit-comment.html" class="item-edit " title="ویرایش"></a>
+                        </td>
+                    </tr>
+
+                </tbody>
+            </table>
+        </div>
+    </div>
+</x-panel-layout>
