8aca0962d190e499b2a8c2460f8788965f43407c
behnamzgh <behnamzgh@gmail.com>
2023-03-06 00:16:24 +0330

    form update va delete va script onha

--- resources/views/panel/comments/index.blade.php
+++ resources/views/panel/comments/index.blade.php
@@ -41,13 +41,31 @@
                             <td>{{ $comment->content }}</td>
                             <td>{{ $comment->jalali_created_at() }}</td>
                             <td>{{ $comment->replies_count }}</td>
-                            <td class="{{ $comment->status ? 'text-success' : 'text-error' }}">{{ $comment->get_status()}}</td>
+                            <td class="{{ $comment->status ? 'text-success' : 'text-error' }}">
+                                {{ $comment->get_status() }}</td>
                             <td>
-                                <a href="" class="item-delete mlg-15" title="حذف"></a>
-                                <a href="show-comment.html" class="item-reject mlg-15" title="رد"></a>
+                                {{-- chon az noe bool hast in shart yani age 1 bod ghesmate rad namayesh bede or else --}}
+                                @if($comment->status)
+                                <a href="" onclick="updateComment(event, {{ $comment->id }})" class="item-reject mlg-15" title="رد"></a>
+                                @else
+                                <a href="" onclick="updateComment(event, {{ $comment->id }})" class="item-confirm mlg-15" title="تایید"></a>
+                                @endif
+
                                 <a href="show-comment.html" target="_blank" class="item-eye mlg-15" title="مشاهده"></a>
-                                <a href="show-comment.html" class="item-confirm mlg-15" title="تایید"></a>
-                                <a href="edit-comment.html" class="item-edit " title="ویرایش"></a>
+
+                                <a href="" class="item-delete mlg-15" onclick="deleteComment(event, {{ $comment->id }})" title="حذف"></a>
+
+                                {{-- form update status --}}
+                                <form action="{{ route('comments.update', $comment->id) }}" method="post" id="update-comment-{{ $comment->id }}">
+                                    @csrf
+                                    @method('put')
+                                </form>
+
+                                {{-- form delete comment --}}
+                                <form action="{{ route('comments.destroy', $comment->id) }}" id="delete-comment-{{ $comment->id }}">
+                                    @csrf
+                                    @method('delete')
+                                </form>
                             </td>
                         </tr>
                     </tbody>
@@ -55,4 +73,34 @@
             </table>
         </div>
     </div>
+    <x-slot name="scripts">
+        <script>
+
+            // script update
+            function updateComment(event, id){
+                event.preventDefault()
+                document.getElementById('update-comment-' + id).submit()
+            }
+
+            // script delete
+            function deleteComment(event, id) {
+                event.preventDefault();
+                Swal.fire({
+                    title: 'آیا مطمئن هستید؟',
+                    text: "بعد از حذف قادر به بازگردانی نیستید...",
+                    icon: 'warning',
+                    showCancelButton: true,
+                    confirmButtonColor: '#d33',
+                    cancelButtonColor: '#3085d6',
+                    confirmButtonText: 'حذف',
+                    cancelButtonText: 'پشیمون شدم'
+                }).then((result) => {
+                    if (result.isConfirmed) {
+                        document.getElementById('delete-comment-' + id).submit();
+                    }
+                })
+            }
+
+        </script>
+    </x-slot>
 </x-panel-layout>
