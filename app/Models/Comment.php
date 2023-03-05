151ba37befe28492b433dcc50e7a526f2b91f588
behnamzgh <behnamzgh@gmail.com>
2023-03-05 23:15:29 +0330

    takmil model comment ha va ravabet dar onha

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -8,4 +8,42 @@ use Illuminate\Database\Eloquent\Model;
 class Comment extends Model
 {
     use HasFactory;
+    protected $guarded = [
+        'id'
+    ];
+
+    // relation baraye namayesh etellate nevisande user
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    // relation baraye namayesh etellate title post
+    public function post()
+    {
+        return $this->belongsTo(Post::class);
+    }
+
+    // relation namayesh etelaate tedade reply roye har comment
+    public function replies()
+    {
+        return $this->hasMany(Comment::class);
+    }
+
+    public function get_status()
+    {
+        return !! $this->status
+                    ? 'تایید شده'
+                    : 'تایید نشده';
+    }
+
+    // function tabdil tarikh miladi b jalali
+    public function jalali_created_at()
+    {
+        return verta($this->created_at)->format('Y/m/d');
+    }
+    public function jalali_updated_at()
+    {
+        return verta($this->updated_at)->format('Y/m/d');
+    }
 }
