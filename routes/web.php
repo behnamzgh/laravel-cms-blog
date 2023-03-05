ec5a707d2f992c398d101bc53be95a47c8fa200b
behnamzgh <behnamzgh@gmail.com>
2023-03-06 01:57:40 +0330

    create profile route and commment profile routed created by breeze package

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\Panel\CommentController;
 use App\Http\Controllers\Panel\UserController;
 use App\Http\Controllers\Panel\PostController;
 use App\Http\Controllers\Panel\EditorUploadController;
+use App\Http\Controllers\Panel\ProfileController as PanelProfileController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -17,7 +18,7 @@ Route::get('/post/{id}', function () {
     return view('post');
 })->name('post.show');
 
-Route::middleware('auth')->get('/karbar', fn () => 'profile')->name('karbar');
+Route::middleware('auth')->get('/profile', [PanelProfileController::class, 'index'])->name('profile');
 
 Route::get('/dashboard', function () {
     return view('panel.index');
@@ -37,10 +38,10 @@ Route::middleware(['auth', 'IsAuthor'])->prefix('/panel')->group(function () {
 });
 
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+// Route::middleware('auth')->group(function () {
+//     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+//     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+//     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+// });
 
 require __DIR__ . '/auth.php';
