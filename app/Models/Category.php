edb5aec7603f4e1cdca2103ba6396df55fddb55f
behnamzgh <behnamzgh@gmail.com>
2023-03-07 21:27:47 +0330

    ijad method children

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -15,9 +15,18 @@ class Category extends Model
         'category_id',
     ];
 
+    // rabete har parent ba child hash yek b chande
+    // yani har child faghat mitone yek parent dashte bashe
     public function parent()
     {
         return $this->belongsTo(Category::class, 'category_id');
+    }
+
+    // rabete har child ba parent chand b yeke yani
+    // har parent mitone koli child zir majmoe khodesh dashte bashe
+    public function children()
+    {
+        return $this->hasMany(Category::class, 'category_id');
     }
 
     public function getParentName()
