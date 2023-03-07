4e21288abf4c9d51df88991837b7a9eadc3fdb8a
behnamzgh <behnamzgh@gmail.com>
2023-03-07 10:58:10 +0330

    takmil method update etellate karbari

--- app/Http/Controllers/Panel/ProfileController.php
+++ app/Http/Controllers/Panel/ProfileController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers\Panel;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Requests\Panel\UpdateProfileRequest;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class ProfileController extends Controller
 {
@@ -11,4 +14,35 @@ class ProfileController extends Controller
     {
         return \view('panel.profile');
     }
+
+    public function update(UpdateProfileRequest $request, User $user)
+    {
+        // 1
+        $data = $request->validated();
+        // 2
+        if($request->hasFile('profile')){
+            $file = $request->file('profile');
+            $ext = $file->getClientOriginalExtension();
+            $file_name = auth()->user()->id . "_" . now() . "." . $ext;
+            $file->storeAs('/images/users', $file_name, 'public_files');
+            $data['profile'] = $file_name;
+        }
+        // 3
+        if($request->password){
+            $data['password'] = Hash::make($request->password);
+        } else {
+            // agar karbar password ersal nekone on ro be soorate null pass mide b DB
+            // k ba unset kardan on inja kolan jolo giri mikonim az update password to DB
+            unset($data['password']);
+        }
+        // 4
+        // on chizi k update mikone in dastoore
+        // jalebe k dare khata migire ro method update ama kar mikone
+        \auth()->user()->update($data);
+        // 5
+        \session()->flash('status', 'اطلااعات کاربری شما به روز شد');
+        return back();
+
+    }
+
 }
