3dbe7dfc652dbfeb2a8fd23bb1781c1fac923e4d
behnamzgh <behnamzgh@gmail.com>
2023-03-09 21:34:23 +0330

    little changes

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -10,9 +10,10 @@ class CommentController extends Controller
     public function store(StoreCommentRequest $request)
     {
         $data = $request->validated();
-        $data['user_id'] = auth()->user()->id;
+        $data['user_id'] = auth()->user()->id; //id karbari k comment dare minevise baraye zakhire to DB va badan namayesh esme on to nazarat
+        // \dd($data);
         Comment::create($data);
-        \session()->flash('status', 'نظر شما ثبت شد و پس اس تایید نمایش داده میشود');
+        \session()->flash('status', 'نظر شما ثبت شد و پس از تایید نمایش داده میشود');
         return \back();
     }
 }
